e48367ea439e510327e1ab25d546d66fdf401554
pipetkaBebra <shurikboy05@gmail.com>
2025-01-20 22:49:40 +0100

    Update UserAccountService.java

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -3,16 +3,19 @@ package com.example.demo.service;
 import com.example.demo.entity.UserAccount;
 import com.example.demo.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserAccountService(UserAccountRepository userAccountRepository) {
+    public UserAccountService(UserAccountRepository userAccountRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userAccountRepository = userAccountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserAccount findByEmail(String email) {
